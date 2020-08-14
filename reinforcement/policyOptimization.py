e660d5e315c47d683ddca615671417613ca8963c
VladMakar <vlad@cratustech.com>
2020-08-14 16:32:24 -0700

    Policy gradients done

--- reinforcement/policyOptimization.py
+++ reinforcement/policyOptimization.py
@@ -12,91 +12,116 @@ import gym
 import torch.nn as nn
 import torch.optim
 import torch.nn.functional as F
+from torch.distributions.categorical import Categorical
+
 
 
 env = gym.make('CartPole-v0')
 
 env.reset()
 
-batch_size = 1000
-epochs = 10
+batch_size = 5000
+epochs = 50
 
 input_size = 4
-hidden_size = 10
+hidden_size = 32
 output_size = 2
+
+device = torch.device("cuda")
 
 policy = nn.Sequential(
     nn.Linear(input_size, hidden_size),
-    nn.ReLU(),
+    # nn.ReLU(),
+    nn.Tanh(),
     nn.Linear(hidden_size, hidden_size),
     nn.ReLU(),
     nn.Linear(hidden_size, output_size),
-    nn.ReLU(),
-)
+    # nn.ReLU(),
+    nn.Sigmoid()
+).to(device)
 
 tobs = env.observation_space.sample()
 print("Action space", env.action_space.n)
-print("test policy out", torch.argmax(F.softmax(policy(torch.as_tensor(tobs, dtype=torch.float32)))).item())
-learning_rate = 1e-4
+print("test policy out", Categorical(policy(torch.as_tensor(tobs, dtype=torch.float32, device=device))).sample().item())
+learning_rate = 1e-2
 optimizer = torch.optim.Adam(policy.parameters(), lr=learning_rate)
 print("action space", env.action_space.sample())
 print("env observation", env.observation_space.sample())
 
 
 # gradient of one trajectory
-def getGrad(policy, trj, rew):
-    expRewrads = []
-    actions    = []
-    for i, sa in enumerate(trj):
+def getGrad(policy, states, rew, actions):
+    expRewrads = [] # [sum(rew)] * len(rew)
+    # sum of the following rewards
+    for i in range(len(rew)):
         expRewrads.append(sum(rew[i:]))
-        actions.append(policy.forward(sa[0]))
-    grad = torch.dot(torch.as_tensor(expRewrads, dtype=torch.float32), torch.as_tensor(actions, dtype=torch.float32))
+    # actions.append(policy.forward(sa[0]))
+    _st = torch.as_tensor(states,  dtype=torch.float32, device=device)
+    _ac = torch.as_tensor(actions, dtype=torch.int32, device=device)
+    out = Categorical(policy(_st)).log_prob(_ac)
+    r = torch.as_tensor(expRewrads, dtype=torch.float32, device=device)
+    grad = -(out * r).mean()
     return grad
 
 
 for n in range(epochs):
     obs = env.reset()
-    obs = torch.as_tensor(obs, dtype=torch.float32)
+    _obs = torch.as_tensor(obs, dtype=torch.float32, device=device)
     trajectories = []
     sa_count = 0
-    state_actions = []
+    states  = []
+    actions = []
     rewards = []
     while True:
         sa_count += 1
-        action = torch.argmax(policy.forward(obs)).item()
-        state_actions.append((obs, action))
+        action = Categorical(policy(_obs)).sample().item()
+        states.append(obs.copy())
+        actions.append(action)
         obs, reward, done, info = env.step(action)
-        obs = torch.as_tensor(obs, dtype=torch.float32)
+        _obs = torch.as_tensor(obs, dtype=torch.float32, device=device)
         rewards.append(reward)
         if done:
-            trajectories.append((state_actions, rewards))
+            trajectories.append((states, rewards, actions))
+            states  = []
+            actions = []
             rewards = []
-            state_actions = []
             obs = env.reset()
-            obs = torch.as_tensor(obs, dtype=torch.float32)
+            _obs = torch.as_tensor(obs, dtype=torch.float32, device=device)
             if sa_count > batch_size:
                 sa_count = 0
                 break
-    print("epoch", n)
+    s = []
+    r = []
+    a = []
     for trj in trajectories:
+        # s += trj[0]
+        # r += trj[1]
+        # a += trj[2]
         policy.zero_grad()
-        grad = getGrad(policy, trj[0], trj[1])
+        grad = getGrad(policy, trj[0], trj[1], trj[2])
         grad.backward()
         optimizer.step()
-        trajectories = []
+    print("avg rewards:", sum(sum(trj[1]) for trj in trajectories)/len(trajectories))
+    trajectories = []
 
 obs = env.reset()
-obs = torch.as_tensor(obs, dtype=torch.float32)
+env.render()
+obs = torch.as_tensor(obs, dtype=torch.float32, device=device)
 print("testing")
-for _ in range(3):
+for _ in range(10):
     rewards = []
-    done = True
-    while done:
+    done = False
+    while not done:
+        env.render()
         # print(obs)
         action = torch.argmax(policy(obs)).item()
         obs, reward, done, info = env.step(action)
-        obs = torch.as_tensor(obs, dtype=torch.float32)
+        obs = torch.as_tensor(obs, dtype=torch.float32, device=device)
         rewards.append(reward)
-    print("test rewards", sum(rewards)/len(rewards))
+        if done:
+            print("test rewards", sum(rewards))
+            rewards = []
+            obs = env.reset()
+            obs = torch.as_tensor(obs, dtype=torch.float32, device=device)
 
 
--- reinforcement/test.py
+++ reinforcement/test.py
@@ -1,25 +1,10 @@
-import torch.nn as nn
-import torch.optim as optim
-import torch
-
-inp = 4
-hidden = 20
-out = 5
-
-policy = nn.Sequential(
-    nn.Linear(inp, hidden),
-    nn.ReLU(),
-    nn.Linear(hidden, hidden),
-    nn.ReLU(),
-    nn.Linear(hidden, out),
-    nn.Sigmoid()
-).cuda()
-device = torch.device("cuda")
-print(device)
-policy.to(device)
 
 
-optimizer = optim.Adam(policy.parameters(), lr=0.001)
+li = []
 
-outs = policy.forward(torch.tensor([0.2, 0.3, 0.4, 0.6], device=device))
+
+li += [1] * 4
+li += [1] * 2
+
+print(li)
 
