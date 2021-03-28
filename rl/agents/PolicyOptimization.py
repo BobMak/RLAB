fixed policy gradients code

rl/agents/PolicyOptimization.py
@@ -17,18 +17,18 @@ class PolicyGradients(Agent):
     def __init__(self, inp, hid, out, isContinuous=False, useLSTM=False, nLayers=1, usewandb=False):
         super(PolicyGradients, self).__init__(inp, hid, out, useLSTM, nLayers, usewandb)
         self.isContinuous = isContinuous
-        # replace the actor layer with an actorCritic layer
+        # replace the discreet output with a continuous Gaussian output
         if isContinuous:
             policy = [*self.model[:-1]]
             self.model = nn.Sequential(
                 *policy,
-                NormalOutput(hid, out, activation=nn.Identity)
+                NormalOutput(hid, out, activation=nn.Tanh)
             ).to(self.device)
         else:
-            policy = [*self.model[:-1]]
+            policy = [*self.model]
             self.model = nn.Sequential(
                 *policy,
-                NormalOutput(hid, out, activation=nn.Identity)
+                # nn.Sigmoid()
             ).to(self.device)
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-2)
 
@@ -52,6 +52,7 @@ class PolicyGradients(Agent):
 
     # gradient of one trajectory
     def backward(self):
+        self.optimizer.zero_grad()
         logProbs = torch.stack(self.logProbs)
         # Compute an advantage
         r = self.trainRewards #- self.avgRewards
@@ -60,8 +61,7 @@ class PolicyGradients(Agent):
         grad = -(logProbs * r).mean()
         grad.backward()
         self.optimizer.step()
-        self.optimizer.zero_grad()
-        print("train reward", self.trainRewards.mean(), "grad", grad, "advtg", r)
+        print("train reward", self.trainRewards.mean(), "grad", grad, "advtg", r.mean())
         self.avgRewards = self.trainRewards.mean()
         # Reset episode buffer
         self.trainRewards = torch.tensor([]).to(self.device)
@@ -72,6 +72,6 @@ class PolicyGradients(Agent):
             self.clearLSTMState()
 
     def __str__(self):
-        return f"PG_h + {super().__str__()}" + ("C" if self.isContinuous else "D")
+        return f"PG_h{super().__str__()}" + ("C" if self.isContinuous else "D")
 
 

rl/utils/EnvHelper.py
@@ -19,7 +19,7 @@ class EnvNormalizer:
 
 
 class EnvHelper:
-    def __init__(self, policy, env, batch_size=5000, epochs=10, normalize=False, batch_is_episode=False):
+    def __init__(self, policy, env, batch_size=5000, epochs=10, normalize=False, batch_is_episode=False, success_reward=None):
         self.policy = policy
         self.epochs = epochs
         self.env = env
@@ -33,6 +33,7 @@ class EnvHelper:
         self._window = 50     # sliding window reward assignment
         self._gamma  = 0.995  # discounted future reward gamma 
         self.setComputeRewardsStrategy("rewardToGoDiscounted")
+        self.success_reward = success_reward
 
     def computeRewards(self, rewards) -> [torch.Tensor]:
         return None
@@ -106,13 +107,16 @@ class EnvHelper:
                 rewards.append(reward)
                 if done:
                     self.policy.saveEpisode(states, actions, self.computeRewards(rewards))
-                    states = []
-                    actions = []
-                    rewards = []
                     obs_raw = self.inputHandler(self.env.reset())
                     obs = torch.from_numpy(obs_raw)
                     obs = torch.as_tensor(obs, dtype=torch.float32, device=self.policy.device)
+                    states  = []
+                    actions = []
+                    rewards = []
                     if sa_count > self.batch_size or self.batch_is_episode:
+                        if self.success_reward and self.success_reward <= sum(rewards):
+                            print(f"policy has reached optimal performance with avg score {sum(rewards)}")
+                            return self.policy
                         break
             self.policy.backward()
         return self.policy