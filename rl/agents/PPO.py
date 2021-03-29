fixed gradients blowing up by detaching the old_log_prob

rl/agents/PPO.py
@@ -11,7 +11,7 @@ from agents.PolicyOptimization import PolicyGradients
 
 
 class PPO(PolicyGradients):
-    def __init__(self, inp, hid, out, clip_ratio=0.2, isContinuous=False, useLSTM=False, nLayers=1, usewandb=False, env=None):
+    def __init__(self, inp, hid, out, clip_ratio=0.02, isContinuous=False, useLSTM=False, nLayers=1, usewandb=False, env=None):
         super().__init__(inp, hid, out, isContinuous, useLSTM, nLayers, usewandb, env)
         self.clip_ratio = clip_ratio
 
@@ -20,25 +20,25 @@ class PPO(PolicyGradients):
         actions = torch.stack(self.train_actions)
         # Compute an advantage
         pred_values = self.getExpectedvalue(self.train_states)
-        r = self.train_rewards - pred_values
+        r = self.train_rewards #- pred_values
         r = (r - r.mean()) / (r.std() + 1e-10).detach()
         if self.use_wandb:
             wandb.log({"avgReward": self.train_rewards.mean()})
             wandb.log({"avgAdvantage": r.mean()})
-        old_log_probs = torch.stack(self.log_probs)
+        old_log_probs = torch.stack(self.log_probs).detach()
         # update actor
-        for _ in range(40):
+        for _ in range(80):
             self.p_optimizer.zero_grad()
             # compute log_probs after the update
             action_distributions = self.getActionDistribution(self.train_states)
             log_probs = action_distributions.log_prob(actions)
             ratio = torch.exp(log_probs - old_log_probs)
             # clip it
-            surr1 = ratio.clamp(min=1 - self.clip_ratio, max=1 + self.clip_ratio) * r
+            surr1 = torch.clamp(ratio, min=1 - self.clip_ratio, max=1 + self.clip_ratio) * r
             surr2 = ratio * r
             grad = torch.min( surr1, surr2 )
-            grad = (-grad).mean()
-            grad.backward(retain_graph=True)
+            grad = -grad.mean()
+            grad.backward()  # retain_graph=True
             self.p_optimizer.step()
         # update critic
         for _ in range(40):

rl/agents/PolicyOptimization.py
@@ -39,7 +39,7 @@ class PolicyGradients(Agent):
             nn.Linear(hid, 1),
             nn.Tanh()
         )
-        self.p_optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-2)
+        self.p_optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-3)
         self.c_optimizer = torch.optim.Adam(self.critic.parameters(), lr=1e-2)
 
         self.log_probs     = []