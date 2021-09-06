766dbbc3771e81efd4b543bd0029cdd77130cdc3
BobMak <vlmr.mko@gmail.com>
2021-09-05 20:05:47 -0700

    temporarily removed normalizer tests due to normalizer being our for vacation

--- rl/test/moduletests.py
+++ rl/test/moduletests.py
@@ -7,26 +7,7 @@ import torch.nn as nn
 import spinup.algos.pytorch.ppo.core as spinnUpCore
 
 import gym
-from utils.EnvHelper import EnvNormalizer
 from utils.Modules import NormalOutput
-
-
-class TestNormalizer(unittest.TestCase):
-    def setUp(self):
-        self.env = gym.make("CartPole-v0")
-        self.env.reset()
-        self.envNorm = EnvNormalizer(self.env)
-
-    def test_varience(self):
-        normzlizedMax = self.envNorm(self.env.observation_space.high)
-        for x in normzlizedMax:
-            self.assertLessEqual(x, 2, f"{x} is > 2")
-        normzlizedMin = self.envNorm(self.env.observation_space.low)
-        for x in normzlizedMin:
-            self.assertGreaterEqual(x, -2, f"{x} is < -2")
-
-    def tearDown(self):
-        self.env.close()
 
 
 class TestLinear(unittest.TestCase):
